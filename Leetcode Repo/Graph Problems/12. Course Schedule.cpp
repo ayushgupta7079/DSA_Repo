5a901a0bddc52f7cf186e1716d435a60dbb1341d
ayushgupta7079 <ayushgupta7079@gmail.com>
2025-10-26 22:57:16 +0530

    Kahn's Algorithm

---
+++ Leetcode Repo/Graph Problems/12. Course Schedule.cpp
@@ -0,0 +1,49 @@
+/*
+207. Course Schedule
+https://leetcode.com/problems/course-schedule/description/
+*/
+
+class Solution
+{
+public:
+  bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+  {
+    int n = numCourses;
+    vector<int> indeg(n, 0);
+    vector<vector<int>> adj(n);
+    for (auto v : prerequisites)
+    {
+      int a = v[1];
+      int b = v[0];
+      indeg[b]++;
+      adj[a].push_back(b);
+    }
+
+    queue<int> q;
+    for (int i = 0; i < n; i++)
+    {
+      if (indeg[i] == 0)
+      {
+        q.push(i);
+      }
+    }
+    vector<int> topo;
+    while (!q.empty())
+    {
+      int node = q.front();
+      q.pop();
+      topo.push_back(node);
+      for (auto v : adj[node])
+      {
+
+        indeg[v]--;
+        if (indeg[v] == 0)
+        {
+          q.push(v);
+        }
+      }
+    }
+
+    return topo.size() == n ? true : false;
+  }
+};
