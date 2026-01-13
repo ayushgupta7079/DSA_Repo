5d93d5b247df6909e62bdc790da8b533f87ab470
AYUSH KUMAR GUPTA <135980863+ayushgupta7079@users.noreply.github.com>
2026-01-13 17:38:05 +0530

    [888] [Time Beats: 43.96%] [Memory Beats: 66.08%] - LeetPush

--- Leetcode Repo/Binary Search/Fair-Candy-Swap.cpp
+++ Leetcode Repo/Binary Search/Fair-Candy-Swap.cpp
@@ -1,29 +1,47 @@
 1class Solution {
 2public:
-3// sumA - gu=ive + take = sumB- take + give -==> take = (sumB-sumA + 2*give)/2
-4    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
-5        
-6
-7        int n = aliceSizes.size();
-8        int m = bobSizes.size();
-9        unordered_map<int,int> mp;
-10
-11        int sumA = 0, sumB = 0;
-12        for(auto v : aliceSizes){
-13            sumA += v;
-14        }
-15        for(auto v : bobSizes){
-16            sumB += v;
-17            mp[v] = 1;
+3    vector<int> isValid(vector<int>& b,  int give, int sumA, int sumB){
+4        int m = b.size();
+5        int lo = 0;
+6        int hi = m-1;
+7        while(lo <= hi){
+8            int mid = lo + (hi-lo)/2;
+9            if(sumA + b[mid]*2 == sumB + 2*give){
+10                return {1, b[mid]};
+11            }
+12            else if(sumA + b[mid]*2 > sumB + 2*give){
+13                hi = mid-1;
+14            }
+15            else{
+16                lo = mid+1;
+17            }
 18        }
-19
-20        for(int i = 0; i < n; i++){
-21            int a = aliceSizes[i];
-22            int req = (sumB-sumA + 2*a)/2;
-23            if(mp[req]){
-24                return {a,req};
-25            }
-26        }
-27        return {0,0};
-28    }
-29};
+19        return {0,-1};
+20    }
+21    // sumA - give + taken == sumB - take + give
+22    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+23        
+24        sort(aliceSizes.begin(),aliceSizes.end());
+25        sort(bobSizes.begin(),bobSizes.end());
+26
+27        int n = aliceSizes.size();
+28        int m = bobSizes.size();
+29
+30        int sumA = 0, sumB = 0;
+31        for(auto v : aliceSizes){
+32            sumA += v;
+33        }
+34        for(auto v : bobSizes){
+35            sumB += v;
+36        }
+37
+38        for(int i = 0; i < n; i++){
+39            int give = aliceSizes[i];
+40            vector<int> valid = isValid(bobSizes,give,sumA,sumB);
+41            if(valid[0]){
+42                return {give, valid[1]};
+43            }
+44        }
+45        return {0,0};
+46    }
+47};
