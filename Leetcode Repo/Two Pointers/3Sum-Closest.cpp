7035410f21e8a5a7c759c8ed823bf47283ccf6b5
AYUSH KUMAR GUPTA <135980863+ayushgupta7079@users.noreply.github.com>
2026-01-23 15:15:23 +0530

    [16] [Time Beats: 76.73%] [Memory Beats: 28.73%] - LeetPush

--- Leetcode Repo/Two Pointers/3Sum-Closest.cpp
+++ Leetcode Repo/Two Pointers/3Sum-Closest.cpp
@@ -12,22 +12,21 @@
 12            int k=n-1;
 13            int sum = 0;
 14            while(i < j && j < k){
-15                
-16                if(nums[i] + nums[j] + nums[k] <= target){
-17                    sum = (nums[i] + nums[j] + nums[k]);
+15                sum = (nums[i] + nums[j] + nums[k]);
+16
+17                if(sum <= target){
 18                    i++;
 19                }
 20                else{
-21                    sum = (nums[i] + nums[j] + nums[k]);
-22                    k--;
-23                }
-24                if(abs(target- sum) < minm){
-25                    ans = sum;
-26                    minm = abs(target - sum);
-27                }
-28            }
-29
-30        }
-31        return ans;
-32    }
-33};
+21                    k--;
+22                }
+23                if(abs(target- sum) < minm){
+24                    ans = sum;
+25                    minm = abs(target - sum);
+26                }
+27            }
+28
+29        }
+30        return ans;
+31    }
+32};
