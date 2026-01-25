dab4a183fab4c4e58c7470f46dff6501f9810404
AYUSH KUMAR GUPTA <135980863+ayushgupta7079@users.noreply.github.com>
2026-01-25 21:16:04 +0530

    [342] [Time Beats: 100.00%] [Memory Beats: 48.04%] - LeetPush

---
+++ Leetcode Repo/Bit Manipulation/Power-of-Four.cpp
@@ -0,0 +1,13 @@
+1class Solution {
+2public:
+3    bool isPowerOfFour(int n) {
+4        int count = __builtin_popcount(n);
+5        // cout<<count<<endl;
+6        if(count != 1) return 0;
+7        
+8        for(int i=0;i<32;i++){
+9            if((n>>i)&1) return i%2==0;
+10        }
+11        return 0;
+12    }
+13};
