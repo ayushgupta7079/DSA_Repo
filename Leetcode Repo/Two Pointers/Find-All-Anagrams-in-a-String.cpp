5e747f5d6a5f53c925f317a6fa66971d8ed0e122
AYUSH KUMAR GUPTA <135980863+ayushgupta7079@users.noreply.github.com>
2026-01-27 21:26:05 +0530

    [438] [Time Beats: 82.62%] [Memory Beats: 61.76%] - LeetPush

---
+++ Leetcode Repo/Two Pointers/Find-All-Anagrams-in-a-String.cpp
@@ -0,0 +1,39 @@
+1class Solution {
+2public:
+3    vector<int> findAnagrams(string s, string p) {
+4        
+5        int n = s.length();
+6        int m = p.length();
+7        if(n < m) return {};
+8        vector<int> srr(26,0), prr(26,0);
+9        vector<int> ans;
+10
+11        for(auto v : p){
+12            prr[v-'a']++;
+13        }
+14
+15        for(int head=0; head<n; head++){
+16
+17            srr[s[head]-'a']++;
+18
+19            if(head >= m){
+20                srr[s[head-m]-'a']--;
+21            }
+22
+23            if(head >= m-1){
+24
+25                bool ana = true;
+26
+27                for(int i=0;i<26;i++){
+28                    if(prr[i] != srr[i]) {
+29                        ana = false; 
+30                        break;
+31                    }
+32                }
+33
+34                if(ana) ans.push_back(head-m+1);
+35            }
+36        }
+37        return ans;
+38    }
+39};
