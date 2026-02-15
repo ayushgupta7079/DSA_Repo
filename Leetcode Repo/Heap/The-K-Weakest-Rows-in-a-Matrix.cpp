9de63944aac509aa09f898a24c5f2dd520dbc2c5
AYUSH KUMAR GUPTA <135980863+ayushgupta7079@users.noreply.github.com>
2026-02-15 14:39:03 +0530

    [1337] [Time Beats: 100.00%] [Memory Beats: 41.40%] - LeetPush

--- Leetcode Repo/Heap/The-K-Weakest-Rows-in-a-Matrix.cpp
+++ Leetcode Repo/Heap/The-K-Weakest-Rows-in-a-Matrix.cpp
@@ -1,28 +1,46 @@
 1class Solution {
 2public:
-3    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-4        
-5        int n = mat.size();
-6        int m = mat[0].size();
-7        priority_queue<pair<int,int>> pq;
-8        for(int i=0;i<n;i++){
-9            int sold = 0;
-10            for(int j=0;j<m;j++){
-11                sold += mat[i][j];
-12            }
-13            pq.push({sold, i});
-14
-15            if(pq.size() > k) pq.pop();
-16        }
-17        vector<int> result;
-18
-19        while(!pq.empty()){
-20            result.push_back(pq.top().second);
-21            pq.pop();
-22        }
-23
-24        reverse(result.begin(), result.end());
-25        return result;
+3    int find(vector<int>& arr){
+4        int n = arr.size();
+5        int lo = 0 ;
+6        int hi = n -1;
+7        int ans = n;
+8        while(lo <= hi){
+9            int mid = lo + (hi - lo)/2;
+10            if(arr[mid] == 0){
+11                ans = mid;
+12                hi = mid-1;
+13            }
+14            else{
+15                lo = mid+1;
+16            }
+17        }
+18        return ans;
+19    }
+20    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+21        
+22        int n = mat.size();
+23        int m = mat[0].size();
+24
+25        vector<int> arr(n);
 26
-27    }
-28};
+27        for(int i= 0 ;i < n ; i++){
+28            arr[i] = find(mat[i]);
+29        }
+30
+31        priority_queue<pair<int,int>> pq;
+32        for(int i=0;i<n;i++){
+33            pq.push({-arr[i],-i});
+34        }
+35
+36        vector<int>ans(k);
+37        int i = 0;
+38        while(i!=k){
+39            ans[i] = -pq.top().second ;
+40            cout<<ans[i]<<endl;
+41            pq.pop();
+42            i++;
+43        }
+44        return ans;
+45    }
+46};
