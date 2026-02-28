93baed00c91c9ef634226a2774ca82c52c023ecd
AYUSH KUMAR GUPTA <135980863+ayushgupta7079@users.noreply.github.com>
2026-03-01 00:18:12 +0530

    [28] [Time Beats: 100.00%] [Memory Beats: 8.30%] - LeetPush

---
+++ Leetcode Repo/String/Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
@@ -0,0 +1,25 @@
+1class Solution {
+2public:
+3    int strStr(string haystack, string needle) {
+4        int m = needle.length();
+5        
+6        string s = needle + "#" + haystack;
+7        int n= s.length();
+8        vector<int> lps(n+1);
+9        lps[0] = -1;
+10
+11        int i=0,j=-1;
+12
+13        while(i < n){
+14            while(j!=-1 && s[i] != s[j]) j = lps[j];
+15            i++;
+16            j++;
+17            lps[i] = j;
+18        }
+19
+20        for(int i=1;i<=n;i++){
+21            if(lps[i] == m) return i-2*m-1;
+22        }
+23        return -1;
+24    }
+25};
